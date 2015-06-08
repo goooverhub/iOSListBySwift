64ef1dab5f7be5b479826c7d0a0987602e98d16a
Lin Shi <shilinsias@gmail.com>
2015-06-07 21:15:01 -0700

    first commit

---
+++ FirstSwiftApp/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  FirstSwiftApp
+//
+//  Created by ShiLin on 6/7/15.
+//  Copyright (c) 2015 ShiLin. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+   
+}
